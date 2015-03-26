232a735b751828b9f65fd23223d884ccfb38c943
Salomon BRYS <salomon.brys@gmail.com>
2015-03-26 18:37:16 +0100

    private setter instead of duplicated property

--- src/main/kotlin/rx/lang/kotlin/subscribers.kt
+++ src/main/kotlin/rx/lang/kotlin/subscribers.kt
@@ -25,13 +25,13 @@ public class FunctionSubscriber<T>(onCompletedFunction: () -> Unit, onErrorFunct
 }
 
 public class FunctionSubscriberModifier<T>(init: FunctionSubscriber<T> = subscriber()) {
-    private var instance: FunctionSubscriber<T> = init
-    public val subscriber: FunctionSubscriber<T> get() = instance
+    public var subscriber: FunctionSubscriber<T> = init
+        private set
 
-    fun onCompleted(onCompletedFunction: () -> Unit) : Unit { instance = instance.onCompleted(onCompletedFunction) }
-    fun onError(onErrorFunction: (t : Throwable) -> Unit) : Unit { instance = instance.onError(onErrorFunction) }
-    fun onNext(onNextFunction: (t : T) -> Unit) : Unit { instance = instance.onNext(onNextFunction) }
-    fun onStart(onStartFunction : () -> Unit) : Unit { instance = instance.onStart(onStartFunction) }
+    fun onCompleted(onCompletedFunction: () -> Unit) : Unit { subscriber = subscriber.onCompleted(onCompletedFunction) }
+    fun onError(onErrorFunction: (t : Throwable) -> Unit) : Unit { subscriber = subscriber.onError(onErrorFunction) }
+    fun onNext(onNextFunction: (t : T) -> Unit) : Unit { subscriber = subscriber.onNext(onNextFunction) }
+    fun onStart(onStartFunction : () -> Unit) : Unit { subscriber = subscriber.onStart(onStartFunction) }
 }
 
 public fun <T> subscriber(): FunctionSubscriber<T> = FunctionSubscriber({}, {throw OnErrorNotImplementedException(it)}, {}, {})
