52f995cb4f5eb6f2a6720dc69eac6c792bd5068a
Prat <banqfirn@gmail.com>
2016-02-17 06:33:05 -0500

    Add support for Single

---
+++ src/test/kotlin/rx/lang/kotlin/SingleTest.kt
@@ -0,0 +1,46 @@
+package rx.lang.kotlin
+
+import org.mockito.Mockito
+import java.util.concurrent.Callable
+import org.junit.Test as test
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.mock
+
+class SingleTest : KotlinTests() {
+    @test fun testCreate() {
+        single<String> { s ->
+            s.onSuccess("Hello World!");
+        }.subscribe { result ->
+            a.received(result)
+        }
+        verify(a, Mockito.times(1)).received("Hello World!")
+    }
+
+    @test fun testCreateFromFuture() {
+        val future = "Hello World!".toSingletonObservable().toBlocking().toFuture()
+        val single = future.toSingle()
+        single.subscribe { result ->
+            a.received(result)
+        }
+        verify(a, Mockito.times(1)).received("Hello World!")
+    }
+
+    @test fun testCreateFromCallable() {
+        val callable = mock(Callable::class.java)
+
+        Mockito.`when`(callable.call()).thenReturn("value")
+
+        callable.toSingle().subscribe { result ->
+            a.received(result)
+        }
+
+        verify(a, Mockito.times(1)).received("value")
+    }
+
+    @test fun testCreateFromJust() {
+        singleOf("Hello World!").subscribe { result ->
+            a.received(result)
+        }
+        Mockito.verify(a, Mockito.times(1)).received("Hello World!")
+    }
+}
