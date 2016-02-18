6eb9b233d1fa3a4e5c1686477f3a95ae9b4583d0
Marcin Koziński <marcin.kozinski@sky.uk>
2016-02-18 22:46:11 +0000

    Make behavior spelling consistent
    Resolves #40

--- src/main/kotlin/rx/lang/kotlin/subjects.kt
+++ src/main/kotlin/rx/lang/kotlin/subjects.kt
@@ -3,8 +3,8 @@ package rx.lang.kotlin
 import rx.schedulers.TestScheduler
 import rx.subjects.*
 
-fun <T> BehaviourSubject() : BehaviorSubject<T> = BehaviorSubject.create()
-fun <T> BehaviourSubject(default : T) : BehaviorSubject<T> = BehaviorSubject.create(default)
+fun <T> BehaviorSubject() : BehaviorSubject<T> = BehaviorSubject.create()
+fun <T> BehaviorSubject(default : T) : BehaviorSubject<T> = BehaviorSubject.create(default)
 fun <T> AsyncSubject() : AsyncSubject<T> = AsyncSubject.create()
 fun <T> PublishSubject() : PublishSubject<T> = PublishSubject.create()
 fun <T> ReplaySubject(capacity : Int = 16) : ReplaySubject<T> = ReplaySubject.create(capacity)
