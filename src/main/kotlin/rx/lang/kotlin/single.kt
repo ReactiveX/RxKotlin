a3f53c26e19d8d7700465968f73bd62bf48845c3
Thomas Nield <thomasnield@live.com>
2017-03-03 19:19:34 -0600

    get rid of single() function

--- src/main/kotlin/rx/lang/kotlin/single.kt
+++ src/main/kotlin/rx/lang/kotlin/single.kt
@@ -5,7 +5,6 @@ import rx.SingleSubscriber
 import java.util.concurrent.Callable
 import java.util.concurrent.Future
 
-inline fun <T> single(crossinline body: (s: SingleSubscriber<in T>) -> Unit): Single<T> = Single.create { body(it) }
 fun <T> T.toSingle(): Single<T> = Single.just(this)
 fun <T> Future<T>.toSingle(): Single<T> = Single.from(this)
 fun <T> Callable<T>.toSingle(): Single<T> = Single.fromCallable { this.call() }
