bce2d7c213b9a61dc9b13f50c8dd0b77242b090e
Thomas Nield <thomasnield@live.com>
2017-03-15 09:10:42 -0500

    remove import  io.reactivex.*

--- src/main/kotlin/io/reactivex/rxkotlin/subscribers.kt
+++ src/main/kotlin/io/reactivex/rxkotlin/subscribers.kt
@@ -1,6 +1,10 @@
 package io.reactivex.rxkotlin
 
-import io.reactivex.*
+import io.reactivex.Completable
+import io.reactivex.Flowable
+import io.reactivex.Maybe
+import io.reactivex.Observable
+import io.reactivex.Single
 import io.reactivex.disposables.Disposable
 import java.lang.RuntimeException
 
