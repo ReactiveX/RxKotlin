9e825f78d61989dcc67d8476afab5ce5d714b0a4
MarcinMoskala <marcinmoskala@gmail.com>
2017-03-20 09:00:59 +0100

    Make Subscribers immutable

--- src/main/kotlin/rx/lang/kotlin/subscribers.kt
+++ src/main/kotlin/rx/lang/kotlin/subscribers.kt
@@ -7,11 +7,12 @@ import rx.observers.SerializedSubscriber
 import rx.subscriptions.Subscriptions
 import java.util.*
 
-class FunctionSubscriber<T>() : Subscriber<T>() {
-    private val onCompletedFunctions = ArrayList<() -> Unit>()
-    private val onErrorFunctions = ArrayList<(e: Throwable) -> Unit>()
-    private val onNextFunctions = ArrayList<(value: T) -> Unit>()
-    private val onStartFunctions = ArrayList<() -> Unit>()
+class FunctionSubscriber<T>(
+        private val onCompletedFunctions: List<() -> Unit> = emptyList(),
+        private val onErrorFunctions: List<(e: Throwable) -> Unit> = emptyList(),
+        private val onNextFunctions: List<(value: T) -> Unit> = emptyList(),
+        private val onStartFunctions: List<() -> Unit> = emptyList()
+) : Subscriber<T>() {
 
     override fun onCompleted() = onCompletedFunctions.forEach { it() }
 
@@ -27,27 +28,39 @@ class FunctionSubscriber<T>() : Subscriber<T>() {
 
     override fun onStart() = onStartFunctions.forEach { it() }
 
-    fun onCompleted(onCompletedFunction: () -> Unit): FunctionSubscriber<T> = copy { onCompletedFunctions.add(onCompletedFunction) }
-    fun onError(onErrorFunction: (t: Throwable) -> Unit): FunctionSubscriber<T> = copy { onErrorFunctions.add(onErrorFunction) }
-    fun onNext(onNextFunction: (t: T) -> Unit): FunctionSubscriber<T> = copy { onNextFunctions.add(onNextFunction) }
-    fun onStart(onStartFunction : () -> Unit) : FunctionSubscriber<T> = copy { onStartFunctions.add(onStartFunction) }
+    fun onCompleted(onCompletedFunction: () -> Unit): FunctionSubscriber<T> = copy(
+            onCompletedFunctions = onCompletedFunctions + onCompletedFunction
+    )
 
-    private fun copy(block: FunctionSubscriber<T>.() -> Unit): FunctionSubscriber<T> {
-        val newSubscriber = FunctionSubscriber<T>()
-        newSubscriber.onCompletedFunctions.addAll(onCompletedFunctions)
-        newSubscriber.onErrorFunctions.addAll(onErrorFunctions)
-        newSubscriber.onNextFunctions.addAll(onNextFunctions)
-        newSubscriber.onStartFunctions.addAll(onStartFunctions)
+    fun onError(onErrorFunction: (t: Throwable) -> Unit): FunctionSubscriber<T> = copy(
+            onErrorFunctions = onErrorFunctions + onErrorFunction
+    )
 
-        newSubscriber.block()
+    fun onNext(onNextFunction: (t: T) -> Unit): FunctionSubscriber<T> = copy(
+            onNextFunctions = onNextFunctions + onNextFunction
+    )
 
-        return newSubscriber
-    }
+    fun onStart(onStartFunction: () -> Unit): FunctionSubscriber<T> = copy(
+            onStartFunctions = onStartFunctions + onStartFunction
+    )
+
+    private fun copy(
+            onCompletedFunctions: List<() -> Unit> = this.onCompletedFunctions,
+            onErrorFunctions: List<(e: Throwable) -> Unit> = this.onErrorFunctions,
+            onNextFunctions: List<(value: T) -> Unit> = this.onNextFunctions,
+            onStartFunctions: List<() -> Unit> = this.onStartFunctions
+    ): FunctionSubscriber<T> = FunctionSubscriber(
+            onCompletedFunctions,
+            onErrorFunctions,
+            onNextFunctions,
+            onStartFunctions
+    )
 }
 
-class FunctionSingleSubscriber<T>() : SingleSubscriber<T>() {
-    private val onSuccessFunctions = ArrayList<(value: T) -> Unit>()
-    private val onErrorFunctions = ArrayList<(e: Throwable) -> Unit>()
+class FunctionSingleSubscriber<T>(
+        private val onSuccessFunctions: List<(value: T) -> Unit> = emptyList(),
+        private val onErrorFunctions: List<(e: Throwable) -> Unit> = emptyList()
+) : SingleSubscriber<T>() {
 
     override fun onSuccess(t: T) = onSuccessFunctions.forEach { it(t) }
 
@@ -59,36 +72,57 @@ class FunctionSingleSubscriber<T>() : SingleSubscriber<T>() {
         }
     }
 
-    fun onSuccess(onSuccessFunction: (t: T) -> Unit): FunctionSingleSubscriber<T> = copy { onSuccessFunctions.add(onSuccessFunction) }
-    fun onError(onErrorFunction: (e: Throwable) -> Unit): FunctionSingleSubscriber<T> = copy { onErrorFunctions.add(onErrorFunction) }
+    fun onSuccess(onSuccessFunction: (t: T) -> Unit): FunctionSingleSubscriber<T> = copy(
+            onSuccessFunctions = onSuccessFunctions + onSuccessFunction
+    )
 
-    private fun copy(block: FunctionSingleSubscriber<T>.() -> Unit): FunctionSingleSubscriber<T> {
-        val newSubscriber = FunctionSingleSubscriber<T>()
-        newSubscriber.onSuccessFunctions.addAll(onSuccessFunctions)
-        newSubscriber.onErrorFunctions.addAll(onErrorFunctions)
+    fun onError(onErrorFunction: (e: Throwable) -> Unit): FunctionSingleSubscriber<T> = copy(
+            onErrorFunctions = onErrorFunctions + onErrorFunction
+    )
 
-        newSubscriber.block()
-
-        return newSubscriber
-    }
+    private fun copy(
+            onSuccessFunctions: List<(value: T) -> Unit> = this.onSuccessFunctions,
+            onErrorFunctions: List<(e: Throwable) -> Unit> = this.onErrorFunctions
+    ): FunctionSingleSubscriber<T> = FunctionSingleSubscriber(
+            onSuccessFunctions,
+            onErrorFunctions
+    )
 }
 
 class FunctionSubscriberModifier<T>(init: FunctionSubscriber<T> = subscriber()) {
+
     var subscriber: FunctionSubscriber<T> = init
         private set
 
-    fun onCompleted(onCompletedFunction: () -> Unit) : Unit { subscriber = subscriber.onCompleted(onCompletedFunction) }
-    fun onError(onErrorFunction: (t : Throwable) -> Unit) : Unit { subscriber = subscriber.onError(onErrorFunction) }
-    fun onNext(onNextFunction: (t : T) -> Unit) : Unit { subscriber = subscriber.onNext(onNextFunction) }
-    fun onStart(onStartFunction : () -> Unit) : Unit { subscriber = subscriber.onStart(onStartFunction) }
+    fun onCompleted(onCompletedFunction: () -> Unit): Unit {
+        subscriber = subscriber.onCompleted(onCompletedFunction)
+    }
+
+    fun onError(onErrorFunction: (t: Throwable) -> Unit): Unit {
+        subscriber = subscriber.onError(onErrorFunction)
+    }
+
+    fun onNext(onNextFunction: (t: T) -> Unit): Unit {
+        subscriber = subscriber.onNext(onNextFunction)
+    }
+
+    fun onStart(onStartFunction: () -> Unit): Unit {
+        subscriber = subscriber.onStart(onStartFunction)
+    }
 }
 
 class FunctionSingleSubscriberModifier<T>(init: FunctionSingleSubscriber<T> = singleSubscriber()) {
+
     var subscriber: FunctionSingleSubscriber<T> = init
         private set
 
-    fun onSuccess(onSuccessFunction: (t: T) -> Unit): Unit { subscriber = subscriber.onSuccess(onSuccessFunction) }
-    fun onError(onErrorFunction: (r: Throwable) -> Unit): Unit {subscriber = subscriber.onError(onErrorFunction) }
+    fun onSuccess(onSuccessFunction: (t: T) -> Unit): Unit {
+        subscriber = subscriber.onSuccess(onSuccessFunction)
+    }
+
+    fun onError(onErrorFunction: (r: Throwable) -> Unit): Unit {
+        subscriber = subscriber.onError(onErrorFunction)
+    }
 }
 
 fun <T> subscriber(): FunctionSubscriber<T> = FunctionSubscriber()
