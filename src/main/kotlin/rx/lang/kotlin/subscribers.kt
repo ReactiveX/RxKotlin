e6e1801ec8a875f5892f0d20ffdf3d3e37961fda
AJ Alt <ajalt@users.noreply.github.com>
2017-05-17 06:56:13 -0700

    Add BlockingObservable.subscribeBy. (#118)

--- src/main/kotlin/rx/lang/kotlin/subscribers.kt
+++ src/main/kotlin/rx/lang/kotlin/subscribers.kt
@@ -5,6 +5,7 @@ import rx.Observable
 import rx.Single
 import rx.Subscription
 import rx.exceptions.OnErrorNotImplementedException
+import rx.observables.BlockingObservable
 
 private val onNextStub: (Any) -> Unit = {}
 private val onErrorStub: (Throwable) -> Unit = { throw OnErrorNotImplementedException(it) }
@@ -21,6 +22,15 @@ fun <T : Any> Observable<T>.subscribeBy(
 
 /**
  * Overloaded subscribe function that allows passing named parameters
+ */
+fun <T : Any> BlockingObservable<T>.subscribeBy(
+        onNext: (T) -> Unit = onNextStub,
+        onError: (Throwable) -> Unit = onErrorStub,
+        onCompleted: () -> Unit = onCompleteStub
+) = subscribe(onNext, onError, onCompleted)
+
+/**
+ * Overloaded subscribe function that allow passing named parameters
  */
 fun <T : Any> Single<T>.subscribeBy(
         onSuccess: (T) -> Unit = onNextStub,
