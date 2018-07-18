f189e0cffc565f0bd6a2083bd942567e0df1e0c2
Arek Olek <arekolek+git@gmail.com>
2018-07-18 13:38:54 +0200

    Add Flowables.create with lambda as last parameter (#188) (#190)

--- src/main/kotlin/io/reactivex/rxkotlin/Flowables.kt
+++ src/main/kotlin/io/reactivex/rxkotlin/Flowables.kt
@@ -2,7 +2,9 @@
 
 package io.reactivex.rxkotlin
 
+import io.reactivex.BackpressureStrategy
 import io.reactivex.Flowable
+import io.reactivex.FlowableEmitter
 import io.reactivex.annotations.BackpressureKind
 import io.reactivex.annotations.BackpressureSupport
 import io.reactivex.annotations.CheckReturnValue
@@ -109,6 +111,15 @@ object Flowables {
                                                             source9: Flowable<T9>, crossinline combineFunction: (T1,T2, T3, T4, T5, T6, T7, T8, T9) -> R) =
             Flowable.combineLatest(source1, source2,source3, source4, source5, source6, source7, source8, source9,
                     Function9 { t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7, t8: T8, t9: T9 -> combineFunction(t1,t2, t3, t4, t5, t6, t7, t8, t9) })!!
+
+
+
+
+    @CheckReturnValue
+    @BackpressureSupport(BackpressureKind.SPECIAL)
+    @SchedulerSupport(SchedulerSupport.NONE)
+    inline fun <T> create(mode: BackpressureStrategy, crossinline source: (FlowableEmitter<T>) -> Unit) =
+            Flowable.create<T>({ source(it) }, mode)!!
 
 
 
